a581d9cba23578abfa4fb249629de8bc870feb19
Wen Yang <mywen1234@gmail.com>
2017-04-06 21:33:13 -0400

    Uploaded new processed data.

--- crime-data-process/code/borough_year.py
+++ crime-data-process/code/borough_year.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 	sc = SparkContext()
 	lines = sc.textFile(','.join(sys.argv[1:]))
 	result = lines.mapPartitions(lambda x: reader(x)) \
-		.map(lambda x: (x[13] + ', ' + x[1].split('/')[2], 1) if x[1] and x[13] else ('outliers', 1)) \
+		.map(lambda x: (x[13] + ', ' + x[1].split('/')[2], 1) if x[1] and x[13] and int(x[1].split('/')[2]) > 2005 and int(x[1].split('/')[2]) < 2016 else ('outliers', 1)) \
 		.filter(lambda (x, y): x != 'outliers') \
 		.reduceByKey(lambda x, y: x + y) \
 		.map(lambda (x, y):  x + '\t' + str(y))
--- crime-data-process/code/spec_loc_year.py
+++ crime-data-process/code/spec_loc_year.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
 	sc = SparkContext()
 	lines = sc.textFile(','.join(sys.argv[1:]))
 	result = lines.mapPartitions(lambda x: reader(x)) \
-		.map(lambda x: (x[16] + ', ' + x[1].split('/')[2], 1) if x[16] and (not x[16].isspace()) and x[1] and int(x[1].split('/')[2]) > 2005 and int(x[1].split('/')[2]) < 2016 else ('outliers', 1)) \
+		.map(lambda x: (x[15] + ', ' + x[1].split('/')[2], 1) if x[15] and (not x[15].isspace()) and x[1] and int(x[1].split('/')[2]) > 2005 and int(x[1].split('/')[2]) < 2016 else ('outliers', 1)) \
 		.filter(lambda (x, y): x != 'outliers') \
 		.reduceByKey(lambda x, y: x + y) \
 		.map(lambda (x, y):  x + '\t' + str(y))
