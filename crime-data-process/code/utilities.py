94cfad86efe899299377323664814741b8b56e50
Wen Yang <mywen1234@gmail.com>
2017-04-13 15:04:04 -0400

    Updated utilities.py. Added result of column grouping.

--- crime-data-process/code/utilities.py
+++ crime-data-process/code/utilities.py
@@ -24,7 +24,7 @@ def checkNull(string):
 def checkDate(line,date_format):
     try:
         date = datetime.datetime.strptime(line,date_format)
-        if (date.year <=2015)&(date.year>=2005):
+        if (date.year <= 2015) & (date.year >= 2006):
             return 'VALID'
         else:
             return 'INVALID/Year'
@@ -47,3 +47,76 @@ def checkRegex(line,regex):
         return 'VALID'
     else:
         return 'INVALID'
+
+
+def checkCaseStatus(line):
+    # check the validation of column offense descrption
+    if checkNull(line) == 'VALID':
+        if (line == 'COMPLETED' or line == 'ATTEMPTED'):
+	    return 'VALID'
+        else:
+            return 'INVALID'
+    else: 
+        return 'NULL'
+
+def checkLevelOfOffense(line):
+    # check the validation of offense level
+    if checkNull(line) == 'VALID':
+        if line in {'FELONY', 'MISDEMEANOR', 'VIOLATION'}:
+            return 'VALID'
+        else: 
+            return 'INVALID'
+    else: 
+        return 'NULL'
+
+def checkBorough(line):
+    # check the validation of borough
+    borough = {'MANHATTAN', 'BROOKLYN', 'QUEENS', 'BRONX', 'STATEN ISLAND'}
+    if checkNull(line) == 'VALID':
+        if line in borough:
+            return 'VALID'
+        else:    
+            return 'INVALID'
+    else: 
+        return 'NULL'
+
+def checkSpecificLoc(line):
+    # check the validation of specific location
+    spec_loc = {'FRONT OF', 'INSIDE', 'REAR OF', 'OUTSIDE', 'OPPOSITE OF'}
+    if checkNull(line) == 'VALID':
+        if line in spec_loc:
+            return 'VALID'
+        else:
+            return 'INVALID'
+    else:
+        return 'NULL'
+
+def checkNumber(line):
+    try:
+        number = float(line)
+        return True
+    except:
+        return False
+
+def checkLatitude(lat):
+    # check the validation of latitude
+    lat_range = [40.48, 40.9]
+    if checkNull(lat) == 'VALID':
+        if checkNumber(lat) and lat > lat_range[0] and lat < lat_range[1]:
+            return 'VALID'
+        else:
+            return 'INVALID'
+    else:
+        return 'NULL'
+
+def checkLongitude(lon):
+    # check the validation of longitude
+    lon_range = [-74.3, -73.6]
+    if checkNull(lat) == 'VALID':
+        if checkNumber(lon) and lon > lon_range[0] and lon < lon_range[1]:
+            return 'VALID'
+        else:
+            return 'INVALID'
+    else:
+        return 'NULL'
+
