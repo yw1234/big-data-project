9272ea8d609f2a2be22a29068fc4450923458397
Wen Yang <mywen1234@gmail.com>
2017-04-15 15:57:12 -0400

    Minor updated.

--- crime-data-process/code/coord_year.py
+++ crime-data-process/code/coord_year.py
@@ -3,13 +3,14 @@ from csv import reader
 from pyspark import SparkContext
 
 if __name__ == "__main__":
-	if len(sys.argv) != 3:
+	if len(sys.argv) != 4:
 		exit(-1)
 	sc = SparkContext()
 	year = sys.argv[1]
-	lines = sc.textFile(','.join(sys.argv[2:]))
+	level = sys.argv[2]
+	lines = sc.textFile(','.join(sys.argv[3:]))
 	result = lines.mapPartitions(lambda x: reader(x)) \
-		.map(lambda x: ('['+ x[21] + ',' + x[22] + ']', 1) if len(x) > 22 and x[21] and x[22] and x[1] and x[1].split('/')[2] == year else ('outliers', 1)) \
+		.map(lambda x: ('['+ x[21] + ',' + x[22] + ']', 1) if len(x) > 22 and x[21] and x[22] and x[1] and x[1].split('/')[2] == year and x[11] and x[11] == level else ('outliers', 1)) \
 		.filter(lambda (x, y): x != 'outliers') \
 		.reduceByKey(lambda x, y: x + y) \
 		.map(lambda (x, y): x + ',')
