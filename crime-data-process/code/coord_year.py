ef8c0a221a85141932538c8644f099ff607cadda
Wen Yang <mywen1234@gmail.com>
2017-04-15 15:18:07 -0400

    Minor updated.

--- crime-data-process/code/coord_year.py
+++ crime-data-process/code/coord_year.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 	year = sys.argv[1]
 	lines = sc.textFile(','.join(sys.argv[2:]))
 	result = lines.mapPartitions(lambda x: reader(x)) \
-		.map(lambda x: (x[21] + ' ' + x[22], 1) if len(x) > 22 and x[21] and x[22] and x[1] and x[1].split('/')[2] == year else ('outliers', 1)) \
+		.map(lambda x: ('['+ x[21] + ',' + x[22] + ']', 1) if len(x) > 22 and x[21] and x[22] and x[1] and x[1].split('/')[2] == year else ('outliers', 1)) \
 		.filter(lambda (x, y): x != 'outliers') \
 		.reduceByKey(lambda x, y: x + y) \
 		.map(lambda (x, y): x + ',')
