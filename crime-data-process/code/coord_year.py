0239fed379149f35ff4cfede0caf37f8462cd0cd
Wen Yang <mywen1234@gmail.com>
2017-04-15 16:16:34 -0400

    Test.

--- crime-data-process/code/coord_year.py
+++ crime-data-process/code/coord_year.py
@@ -10,10 +10,10 @@ if __name__ == "__main__":
 	level = sys.argv[2]
 	lines = sc.textFile(','.join(sys.argv[3:]))
 	result = lines.mapPartitions(lambda x: reader(x)) \
-		.map(lambda x: ('['+ x[21] + ',' + x[22] + ']', 1) if len(x) > 22 and x[21] and x[22] and x[1] and x[1].split('/')[2] == year and x[11] and x[11] == level else ('outliers', 1)) \
+		.map(lambda x: (x[21] + ',' + x[22], 1) if len(x) > 22 and x[21] and x[22] and x[1] and x[1].split('/')[2] == year and x[11] and (level == 'ALL' or x[11] == level) else ('outliers', 1)) \
 		.filter(lambda (x, y): x != 'outliers') \
 		.reduceByKey(lambda x, y: x + y) \
-		.map(lambda (x, y): x + ',')
+		.map(lambda (x, y): x)
 	result.saveAsTextFile('result.out')
 	sc.stop()	
 
