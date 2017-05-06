bb6f6070ac0def2a3325d633b98d93fa57640510
Wen Yang <mywen1234@gmail.com>
2017-05-06 15:57:21 -0400

    Added weather data process code.

---
+++ crime-data-process/code/data-exploration/weather/inside_2011.py
@@ -0,0 +1,19 @@
+import sys
+from csv import reader
+from pyspark import SparkContext
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		exit(-1)
+	sc = SparkContext()
+	outside = ['STREET', 'BRIDGE', 'CEMETERY', 'MARINA/PIER', 'OPEN AREAS (OPEN LOTS)', 'PARK/PLAYGROUND']
+	lines = sc.textFile(','.join(sys.argv[1:]))
+	result = lines.mapPartitions(lambda x: reader(x)) \
+		.map(lambda x: (x[1], 1) if x[16] and x[16] not in outside and x[1] and int(x[1].split('/')[2]) == 2011 else ('outliers', 1)) \
+		.filter(lambda (x, y): x != 'outliers') \
+		.reduceByKey(lambda x, y: x + y) \
+		.map(lambda (x, y):  x + '\t' + str(y))
+	result.saveAsTextFile('result.out')
+	sc.stop()	
+
+
