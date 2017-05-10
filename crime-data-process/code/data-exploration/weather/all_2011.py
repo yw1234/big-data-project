52b15e32c7bc36134e32527f4d050d67f4a94c3b
yw1234 <mywen1234@gmail.com>
2017-05-10 00:06:31 +0000

    Added all crime count in 2011.

---
+++ crime-data-process/code/data-exploration/weather/all_2011.py
@@ -0,0 +1,19 @@
+import sys
+from csv import reader
+from pyspark import SparkContext
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		exit(-1)
+	sc = SparkContext()
+	lines = sc.textFile(','.join(sys.argv[1:]))
+	result = lines.mapPartitions(lambda x: reader(x)) \
+		.map(lambda x: (x[1], 1) if x[16] and x[16] and x[1] and int(x[1].split('/')[2]) == 2011 else ('outliers', 1)) \
+		.filter(lambda (x, y): x != 'outliers') \
+		.reduceByKey(lambda x, y: x + y) \
+                .sortByKey() \
+		.map(lambda (x, y):  x + '\t' + str(y))
+	result.saveAsTextFile('result.out')
+	sc.stop()	
+
+
